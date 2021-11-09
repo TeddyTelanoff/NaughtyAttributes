3aa069ec60e58e5dbf5c3fd039befe4d4f2e713e
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2021-11-08 18:12:42 -0800

    Change Required Property to use label displayed normally

--- Assets/NaughtyAttributes/Scripts/Editor/PropertyValidators/RequiredPropertyValidator.cs
+++ Assets/NaughtyAttributes/Scripts/Editor/PropertyValidators/RequiredPropertyValidator.cs
@@ -11,8 +11,46 @@ namespace NaughtyAttributes.Editor
 			if (property.propertyType == SerializedPropertyType.ObjectReference)
 			{
 				if (property.objectReferenceValue == null)
+				{using UnityEditor;
+
+// Changed by Treidex to have the default message use the name/label you would normally see on the property in the inspector
+
+using static NaughtyAttributes.Editor.PropertyUtility;
+
+namespace NaughtyAttributes.Editor
+{
+	public class RequiredPropertyValidator : PropertyValidatorBase
+	{
+		public override void ValidateProperty(SerializedProperty property)
+		{
+			RequiredAttribute requiredAttribute = PropertyUtility.GetAttribute<RequiredAttribute>(property);
+
+			if (property.propertyType == SerializedPropertyType.ObjectReference)
+			{
+				if (property.objectReferenceValue == null)
 				{
-					string errorMessage = property.name + " is required";
+					LabelAttribute labelAttribute = GetAttribute<LabelAttribute>(property);
+					string labelText = (labelAttribute == null)
+						? property.displayName
+						: labelAttribute.Label;
+					string errorMessage = labelText + " is required";
+					if (!string.IsNullOrEmpty(requiredAttribute.Message))
+					{
+						errorMessage = requiredAttribute.Message;
+					}
+
+					NaughtyEditorGUI.HelpBox_Layout(errorMessage, MessageType.Error, context: property.serializedObject.targetObject);
+				}
+			}
+			else
+			{
+				string warning = requiredAttribute.GetType().Name + " works only on reference types";
+				NaughtyEditorGUI.HelpBox_Layout(warning, MessageType.Warning, context: property.serializedObject.targetObject);
+			}
+		}
+	}
+}
+
 					if (!string.IsNullOrEmpty(requiredAttribute.Message))
 					{
 						errorMessage = requiredAttribute.Message;
